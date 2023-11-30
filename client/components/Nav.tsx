c44db24050b6f1ae3a769d223ae9320d01f9b7e0
Peter Hind <peter.hind47@gmail.com>
2023-11-30 14:46:13 +1300

    nav update

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -2,7 +2,7 @@ import { useAuth0 } from '@auth0/auth0-react'
 import LoginButton from './LoginButton'
 import LogoutButton from './LogoutButton'
 import Profile from './Profile'
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Nav() {
